3e835a44711a476474eba2b3bff93c75732aa442
karan1149 <karan1149@gmail.com>
2016-12-02 18:30:54 -0800

    minor edit to method signature

--- game.py
+++ game.py
@@ -6,7 +6,7 @@ def startState():
 def actions(state):
 
 # get successor action from a state given an action
-def Succ(state, action):
+def succ(state, action):
 
 # check if a state is an end state
 def isEnd(state):
