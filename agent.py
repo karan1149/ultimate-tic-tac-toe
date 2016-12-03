67d7a00d12bef5e6411e29a0b41ace8df11c44c2
karan1149 <karan1149@gmail.com>
2016-12-02 20:42:09 -0800

    Added reflex agent.

--- agent.py
+++ agent.py
@@ -7,3 +7,24 @@ class RandomAgent:
         numberActions = len(possibleActions);
         randomAction = possibleActions[random.randrange(numberActions)];
         return randomAction;
+class ReflexAgent:
+    def getAction(self, state):
+        possibleActions = actions(state);
+        modifiedActions = [action for action in possibleActions if actionFilter(action)];
+        numberActions = len(modifiedActions);
+        if numberActions != 0:
+            randomAction = modifiedActions[random.randrange(numberActions)];
+        else:
+            numberActions = len(possibleActions);
+            randomAction = possibleActions[random.randrange(numberActions)];
+        return randomAction;
+
+    def actionFilter(self, state, action):
+        currentOppWins = getGridWins(state)[1];
+        # check if making move might give a square to the opponent
+        opponentState = succ(state, action);
+        opponentActions = actions(opponentState);
+        # assume a function returning (Player 1 tiles won, Player 2 tiles won) exists getGridWins(state)
+        opponentSuccess = any([getGridWins(succ(opponentState, oppAction))[1] > currentOppWins for oppAction in opponentActions]);
+        return not opponentSuccess;
+
