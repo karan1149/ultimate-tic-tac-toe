c83d20cb04225314479118017059453821c4add8
karan1149 <karan1149@gmail.com>
2016-12-03 14:04:02 -0800

    Print while simulating for debugging

--- simulate.py
+++ simulate.py
@@ -10,11 +10,15 @@ class TicTacToeSimulator:
     def playGame(self):
         state = startState();
         while not isEnd(state):
+            printBoard(state[0]);
+            print state[1], state[2];
+            print;
             assert(player(state) >= 0 and player(state) <= 1);
             if player(state) == 1:
                 action = self.agent1.getAction(state);
             else:
                 action = self.agent2.getAction(state);
+            print action;
             state = succ(state, action);
         return utility(state);
 
