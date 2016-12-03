fe8bfdd8a011ea41c17e78c9703886b0b47cca93
karan1149 <karan1149@gmail.com>
2016-12-02 18:29:12 -0800

    Interface for game specification

---
+++ game.py
@@ -0,0 +1,18 @@
+
+# get start state for game
+def startState():
+
+# get possible actions for a given state
+def actions(state):
+
+# get successor action from a state given an action
+def Succ(state, action):
+
+# check if a state is an end state
+def isEnd(state):
+
+# utility for state if it is an end state
+def utility(state):
+
+# player that needs to move in a given state
+def player(state):
