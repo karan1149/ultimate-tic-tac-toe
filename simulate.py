606d041ba612a52c038909c31ed5247281e2e0c6
karan1149 <karan1149@gmail.com>
2016-12-02 19:27:18 -0800

    Simulator that simulates 1 or more games between 2 agents

---
+++ simulate.py
@@ -0,0 +1,26 @@
+import game as *;
+import agent as *;
+
+class TicTacToeSimulator:
+    __init__(self, Agent1, Agent2):
+        self.agent1 = Agent1;
+        self.agent2 = Agent2;
+
+    # play a single game and return the winner
+    playGame(self):
+        state = startState();
+        while not isEnd(state):
+            if player(state) == 1:
+                action = Agent1.getAction(state);
+            elif player(state) == 2:
+                action = Agent2.getAction(state);
+            state = succ(state, action);
+
+        return 1 if utility(state) > 0 else 2;
+
+    # play n games and return the percentage of times Agent1 won
+    playGames(self, n):
+        won = 0.0;
+        for i in range(n):
+            won += 1 if self.playGame() == 1 else 0;
+        return won / n;
