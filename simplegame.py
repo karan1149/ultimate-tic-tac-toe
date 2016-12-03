4eb625b4c46705a07d79222bf2396a0c637e1c2a
Varun Nambikrishnan <varun14@stanford.edu>
2016-12-03 15:27:17 -0800

    succ method

--- simplegame.py
+++ simplegame.py
@@ -1,4 +1,5 @@
 from util import *
+import copy
 
 # get start state for game
 def startState():
@@ -11,7 +12,7 @@ def startState():
 
 ## get possible actions from current state
 def actions(state):
-	actions = []
+	actions = [] ## possible positions the player can play in
 	for rowNumber, row in enumerate(state[0]):
 		for colNumber, col in enumerate(row):
 			if(col == 0):
@@ -22,10 +23,16 @@ def actions(state):
 
 ## get successor state given a current state and an action
 def succ(state, action):
-	
+	board = copy.deepcopy(state[0])
+	rowCol = rowColFromPos(action)
+	board[rowCol[0]][rowCol[1]] = state[1] + 1
+	return (board, getOppIndex(state[1]))
+
+
 
 ## check if a state is an end state
 def isEnd(state):
 
 ## utility of state, given that it is an end state
 def utility(state):
+	
