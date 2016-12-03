9488713b89c18ea9b836786e7c9dc7ed1b21d7da
karan1149 <karan1149@gmail.com>
2016-12-02 21:16:43 -0800

    cleaning up simulator code

--- simulate.py
+++ simulate.py
@@ -2,23 +2,23 @@ import game as *;
 import agent as *;
 
 class TicTacToeSimulator:
-    def __init__(self, Agent1, Agent2):
-        self.agent1 = Agent1;
-        self.agent2 = Agent2;
+    def __init__(self, agent1, agent2):
+        self.agent1 = agent1;
+        self.agent2 = agent2;
 
     # play a single game and return the winner
     def playGame(self):
         state = startState();
         while not isEnd(state):
             if player(state) == 1:
-                action = Agent1.getAction(state);
+                action = agent1.getAction(state);
             elif player(state) == 2:
-                action = Agent2.getAction(state);
+                action = agent2.getAction(state);
             state = succ(state, action);
 
         return 1 if utility(state) > 0 else 2;
 
-    # play n games and return the percentage of times Agent1 won
+    # play n games and return the percentage of times agent1 won
     def playGames(self, n):
         won = 0.0;
         for i in range(n):
