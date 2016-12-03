419aba638fc531eb5c8ac4c3f2c759e68aa413b4
Varun Nambikrishnan <varun14@stanford.edu>
2016-12-03 00:19:22 -0800

    working on print

--- util.py
+++ util.py
@@ -1,70 +1,77 @@
 def dotProduct(v1, v2):
-    sum = 0;
-    for x in v1:
-        sum += v1[x] * v2[x]
-    return sum;
+	sum = 0;
+	for x in v1:
+		sum += v1[x] * v2[x]
+	return sum;
 
 def incrementSparseVector(v1, scale, v2):
-    for x in v2:
-        v1[x] += v2[x] * scale;
+	for x in v2:
+		v1[x] += v2[x] * scale;
 
 def multiplySparseVector(v1, scale):
-    for x in v1:
-        v1[x] *= scale;
+	for x in v1:
+		v1[x] *= scale;
 
 def getOppIndex(playerIndex):
-    return abs(playerIndex - 1);
+	return abs(playerIndex - 1);
 
+def printBoard(board):
+	for i in range(3):
+		curGrid1 = board[i*3]
+		curGrid2 = board[i*3 + 1] 
+		curGrid3 = board[i*3 + 2] 
+		 
 def getGridWins(state): # returns tuple of (agentWins, oppWins)
-    agentWins = 0
-    oppWins = 0
-    for grid in state[0]: #state[0] = board
-        if(grid.status == 1):
-            agentWins += 1
-        if(grid.status == 2):
-            oppWins += 1
-    return (agentWins, oppWins)
+	agentWins = 0
+	oppWins = 0
+	for grid in state[0]: #state[0] = board
+		if(grid.status == 1):
+			agentWins += 1
+		if(grid.status == 2):
+			oppWins += 1
+	return (agentWins, oppWins)
 
 def posFromRowCol(row, col):
-    return 3*row + col
+	return 3*row + col
 
 def rowColFromPos(pos):
-    row = pos/3
-    col = pos % 3
-    return (row, col)
+	row = pos/3
+	col = pos % 3
+	return (row, col)
 
 def randomMax(successors):
-    maximumValue = max(successors)[0];
-    possibleSuccessors = [successor for successor in successors if successor[0] == maximumValue];
-    return random.choice(possibleSuccessors)[1];
+	maximumValue = max(successors)[0];
+	possibleSuccessors = [successor for successor in successors if successor[0] == maximumValue];
+	return random.choice(possibleSuccessors)[1];
 
 def countCenterMoves(state, player):
-    centerMoves = 0
-    board = state[0]
-    for grid in board:
-        if(grid[1][1] == player + 1):
-            centerMoves += 1
-    return centerMoves
+	centerMoves = 0
+	board = state[0]
+	for grid in board:
+		if(grid[1][1] == player + 1):
+			centerMoves += 1
+	return centerMoves
 
 def countCornerMoves(state, player):
-    cornerMoves = 0
-    board = state[0]
-    for grid in board:
-        if(grid[0][0] == player + 1):
-            cornerMoves += 1
-        if(grid[0][2] == player + 1):
-            cornerMoves += 1
-        if(grid[2][0] == player + 1):
-            cornerMoves += 1
-        if(grid[2][2] == player + 1):
-            cornerMoves += 1
-    return cornerMoves
+	cornerMoves = 0
+	board = state[0]
+	for grid in board:
+		if(grid[0][0] == player + 1):
+			cornerMoves += 1
+		if(grid[0][2] == player + 1):
+			cornerMoves += 1
+		if(grid[2][0] == player + 1):
+			cornerMoves += 1
+		if(grid[2][2] == player + 1):
+			cornerMoves += 1
+	return cornerMoves
 
 def countAdjacentMoves(state, player):
-    adjMoves = 0
-    board = state[0]
-    for grid in board:
-        for row in grid:
-            for col in grid[row]:
-                
+	adjMoves = 0
+	board = state[0]
+	for grid in board:
+		for row in grid:
+			for col in grid[row]:
+
+
 
