083cde3dfd96f111c6a3a77e48d503c5767466b4
Varun Nambikrishnan <varun14@stanford.edu>
2016-12-03 16:00:40 -0800

    finished isEnd, utility, and player for simpleGame

--- simplegame.py
+++ simplegame.py
@@ -8,9 +8,9 @@ def startState():
 		board.append([])
 		for j in range(3):
 			board[i].append(0)
-	return (board, 0)
+	return (board, 0) ## tuple of board and player's turn
 
-## get possible actions from current state
+## get possible actions from current state = positions open on board
 def actions(state):
 	actions = [] ## possible positions the player can play in
 	for rowNumber, row in enumerate(state[0]):
@@ -28,11 +28,48 @@ def succ(state, action):
 	board[rowCol[0]][rowCol[1]] = state[1] + 1
 	return (board, getOppIndex(state[1]))
 
-
-
-## check if a state is an end state
 def isEnd(state):
+	result = playerWins(state)
+	return result[0]
+
+## Returns a tuple of a boolean of whether the game is over, and who won e.g (True, 1) or (False, None)
+def playerWins(state):
+	grid = state[0]
+	center = grid[1][1]
+	if(center != 0):
+		if((center == grid[1][0] and center == grid[1][2]) or (center == grid[0][1] and center == grid[2][1]) or (center == grid[0][0] and center == grid[2][2]) or (center == grid[0][2] and center == grid[2][0])):
+			return (True, center)
+	topMid = grid[0][1]
+	if(topMid != 0):
+		if(topMid == grid[0][0] and topMid == grid[0][2]):
+			return (True, topMid)
+	leftMid = grid[1][0]
+	if(leftMid != 0):
+		if(leftMid == grid[0][0] and leftMid == grid[2][0]):
+			return (True, leftMid)
+	rightMid = grid[1][2]
+	if(rightMid != 0):
+		if(rightMid == grid[0][2] and rightMid == grid[2][2]):
+			return (True, rightMid)
+	botMid = grid[2][1]
+	if(botMid != 0):
+		if(botMid == grid[2][0] and botMid == grid[2][2]):
+			return(True, botMid)
+	for row in grid:
+		for col in row:
+			if(col == 0):
+				return (False, None)
+	return (True, 3)
 
 ## utility of state, given that it is an end state
 def utility(state):
-	
+	result = playerWins(state)
+	if(result[1] == 1):
+		return 100
+	elif(result[1] == 2):
+		return -100
+	return 0
+## gets which player's turn it is
+def player(state):
+	return state[1]
+
