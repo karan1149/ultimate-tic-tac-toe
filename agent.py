2392bd6544b9e9abe34660fb689932641955956e
karan1149 <karan1149@gmail.com>
2016-12-09 00:58:42 -0800

    Moved some agents around and commented out old minimax code

--- agent.py
+++ agent.py
@@ -11,13 +11,6 @@ class RandomAgent:
         randomAction = possibleActions[random.randrange(numberActions)];
         return randomAction;
 
-class SimpleRandomAgent:
-    def getAction(self, state):
-        possibleActions = simplegame.actions(state);
-        numberActions = len(possibleActions);
-        randomAction = possibleActions[random.randrange(numberActions)];
-        return randomAction;
-
 class PerceptronAgent:
 
     def __init__(self):
@@ -39,27 +32,6 @@ class PerceptronAgent:
         features["numAdjacentPieces"] = countAdjacentMoves(state, getOppIndex(player(state)));
         return features;
 
-class SimplePerceptronAgent:
-
-    def __init__(self):
-        self.weights = {"numWins" : 5, "numCenterPieces": .2, "numAdjacentPieces": .4, "numCornerPieces": .1};
-
-    def getAction(self, state):
-        possibleActions = simplegame.actions(state);
-        scoredActions = [];
-        for action in possibleActions:
-            successor = simplegame.succ(state, action);
-            scoredActions.append((dotProduct(self.simpleFeatureExtractor(successor), self.weights), action));
-        return randomMax(scoredActions);
-
-    def simpleFeatureExtractor(self, state):
-        features = collections.defaultdict(float);
-        otherPlayer = getOppIndex(player(state));
-        features["numWins"] = 1 if simplegame.playerWins(state)[1] == otherPlayer + 1 else 0;
-        features["numCenterPieces"] = 1 if state[0][1][1] == otherPlayer else 0;
-        features["numCornerPieces"] = simpleCountCornerMoves(state, otherPlayer);
-        features["numAdjacentPieces"] = simpleAdjacentMoves(state, otherPlayer);
-        return features;
 
 class ReflexAgent:
     def getAction(self, state):
@@ -84,72 +56,72 @@ class ReflexAgent:
 
         return not opponentSuccess;
 
-class MinimaxAgent:
-    def __init__(self, d):
-        self.depth = d;
-        self.weights = collections.defaultdict(float);
-        # optimize eta, decreasing function
-        self.eta = .0001;
-        # can play with number of iterations
-        self.monteCarloIterations = 25;
-        self.eval = lambda state: dotProduct(state, self.weights);
-    def monteCarloUpdate(self, state):
-        for i in range(self.monteCarloIterations):
-            # can play with exploration policy
-            episode = [];
-            currentState = state;
-            while not isEnd(currentState):
-                player = player(currentState);
-                actions = actions(state);
-                if player == 0:
-                    bestNewState = None;
-                    bestAction = None;
-                    bestScore = float("-inf");
-                    for action in actions:
-                        successor = succ(state, action);
-                        score = dotProduct(featureExtractor(successor), self.weights);
-                        if score > bestScore:
-                            bestAction = action;
-                            bestNewState = successor;
-                    reward = 0 if not isEnd(newState) else utility(newState);
-                    episode.extend((currentState, bestAction, reward, bestNewState));
-                    currentState = bestNewState;
-                elif player == 1:
-                    worstNewState = None;
-                    worstAction = None;
-                    worstScore = float("inf");
-                    for action in actions:
-                        successor = succ(state, action);
-                        score = dotProduct(featureExtractor(successor), self.weights);
-                        if score > worstScore:
-                            worstAction = action;
-                            worstNewState = successor;
-                    reward = 0 if not isEnd(newState) else utility(newState);
-                    episode.extend((currentState, worstAction, reward, worstNewState));
-                    currentState = worstNewState;
-            # possibly shuffle or reverse
-            for resultTuple in episode:
-                TDLearningUpdate(*resultTuple);
-    def TDLearningUpdate(self, state, action, reward, newState):
-        gradient = featureExtractor(state);
-        residual = dotProduct(featureExtractor(state), self.weights) - reward - dotProduct(featureExtractor(newState), self.weights);
-        multiplyVector(gradient, residual);
-        incrementSparseVector(self.weights, -1 * self.eta, gradient);
-    def getAction(self, state):
-        def minimaxValue(state, depth, firstInTree, agent):
-            actions = actions(state);
-            if isEnd(state):
-                return utility(state);
-            elif depth == 0:
-                return self.eval(state);
-            newDepth = depth - 1 if agent != firstInTree else depth;
-            if agent == 0:
-                return max(minimaxValue(succ(state, action), newDepth, firstInTree, getOppIndex(agent)) for action in actions);
-            elif agent == 1:
-                return min(minimaxValue(succ(state, action), newDepth, firstInTree, getOppIndex(agent)) for action in actions);
-        self.monteCarloUpdate(state);
-        return randomMax([(minimaxValue(succ(state, action), self.depth, getOppIndex(player(state)), getOppIndex(player(state))), action) \
-            for action in actions]);
+# class MinimaxAgent:
+#     def __init__(self, d):
+#         self.depth = d;
+#         self.weights = collections.defaultdict(float);
+#         # optimize eta, decreasing function
+#         self.eta = .0001;
+#         # can play with number of iterations
+#         self.monteCarloIterations = 25;
+#         self.eval = lambda state: dotProduct(state, self.weights);
+#     def monteCarloUpdate(self, state):
+#         for i in range(self.monteCarloIterations):
+#             # can play with exploration policy
+#             episode = [];
+#             currentState = state;
+#             while not isEnd(currentState):
+#                 player = player(currentState);
+#                 actions = actions(state);
+#                 if player == 0:
+#                     bestNewState = None;
+#                     bestAction = None;
+#                     bestScore = float("-inf");
+#                     for action in actions:
+#                         successor = succ(state, action);
+#                         score = dotProduct(featureExtractor(successor), self.weights);
+#                         if score > bestScore:
+#                             bestAction = action;
+#                             bestNewState = successor;
+#                     reward = 0 if not isEnd(newState) else utility(newState);
+#                     episode.extend((currentState, bestAction, reward, bestNewState));
+#                     currentState = bestNewState;
+#                 elif player == 1:
+#                     worstNewState = None;
+#                     worstAction = None;
+#                     worstScore = float("inf");
+#                     for action in actions:
+#                         successor = succ(state, action);
+#                         score = dotProduct(featureExtractor(successor), self.weights);
+#                         if score > worstScore:
+#                             worstAction = action;
+#                             worstNewState = successor;
+#                     reward = 0 if not isEnd(newState) else utility(newState);
+#                     episode.extend((currentState, worstAction, reward, worstNewState));
+#                     currentState = worstNewState;
+#             # possibly shuffle or reverse
+#             for resultTuple in episode:
+#                 TDLearningUpdate(*resultTuple);
+#     def TDLearningUpdate(self, state, action, reward, newState):
+#         gradient = featureExtractor(state);
+#         residual = dotProduct(featureExtractor(state), self.weights) - reward - dotProduct(featureExtractor(newState), self.weights);
+#         multiplyVector(gradient, residual);
+#         incrementSparseVector(self.weights, -1 * self.eta, gradient);
+#     def getAction(self, state):
+#         def minimaxValue(state, depth, firstInTree, agent):
+#             actions = actions(state);
+#             if isEnd(state):
+#                 return utility(state);
+#             elif depth == 0:
+#                 return self.eval(state);
+#             newDepth = depth - 1 if agent != firstInTree else depth;
+#             if agent == 0:
+#                 return max(minimaxValue(succ(state, action), newDepth, firstInTree, getOppIndex(agent)) for action in actions);
+#             elif agent == 1:
+#                 return min(minimaxValue(succ(state, action), newDepth, firstInTree, getOppIndex(agent)) for action in actions);
+#         self.monteCarloUpdate(state);
+#         return randomMax([(minimaxValue(succ(state, action), self.depth, getOppIndex(player(state)), getOppIndex(player(state))), action) \
+#             for action in actions]);
 
 def featureExtractor(state):
     features = collections.defaultdict(float);
@@ -178,6 +150,18 @@ def featureExtractor(state):
         if state[1] == 1:
             features["advantageOfNextGrid"] *= -1;
     return features;
+
+class AdvancedPerceptronAgent:
+    def __init__(self):
+        self.weights = {'numCornerPieces': 0.811020200948285, 'numOtherCenterPieces': 0.0809559760347321, 'relativeWins': 1.0153244869861855, 'numAdjacentWonGrids': 0.969296961952278, 'numOtherAdjacentWonGrids': -0.6814807021505367, 'advantageOfNextGrid': 0.5395673542865317, 'numOtherCornerPieces': 0.7058793287408335, 'gridsDifference': 1.6507776641028122, 'numAdjacentPieces': 0.7130969880480094, 'numWins': 0.5323490704404454, 'numCenterPieces': 0.021778991208712777, 'otherWins': -0.48297541654574627, 'numOtherAdjacentPieces': -1.2963960051123138};
+
+    def getAction(self, state):
+        possibleActions = actions(state);
+        scoredActions = [];
+        for action in possibleActions:
+            successor = succ(state, action);
+            scoredActions.append((dotProduct(featureExtractor(successor), self.weights), action));
+        return randomMax(scoredActions);
 
 class MinimaxPruningAgent:
     def __init__(self, d):
@@ -284,19 +268,6 @@ class MinimaxPruningAgent:
         return randomMax([(minimaxValue(succ(state, action), self.depth, getOppIndex(player(state)), getOppIndex(player(state)), float("-inf"), float("inf")), action) \
             for action in actions(state)]);
 
-class AdvancedPerceptronAgent:
-    def __init__(self):
-        self.weights = {'numCornerPieces': 0.811020200948285, 'numOtherCenterPieces': 0.0809559760347321, 'relativeWins': 1.0153244869861855, 'numAdjacentWonGrids': 0.969296961952278, 'numOtherAdjacentWonGrids': -0.6814807021505367, 'advantageOfNextGrid': 0.5395673542865317, 'numOtherCornerPieces': 0.7058793287408335, 'gridsDifference': 1.6507776641028122, 'numAdjacentPieces': 0.7130969880480094, 'numWins': 0.5323490704404454, 'numCenterPieces': 0.021778991208712777, 'otherWins': -0.48297541654574627, 'numOtherAdjacentPieces': -1.2963960051123138};
-
-    def getAction(self, state):
-        possibleActions = actions(state);
-        scoredActions = [];
-        for action in possibleActions:
-            successor = succ(state, action);
-            scoredActions.append((dotProduct(featureExtractor(successor), self.weights), action));
-        return randomMax(scoredActions);
-# do expectimax pruning agent
-
 class ExpectimaxAgent:
     def __init__(self, d):
         self.depth = d;
@@ -332,3 +303,32 @@ class ExpectimaxAgent:
         # self.monteCarloUpdate(state);
         return randomMax([(minimaxValue(succ(state, action), self.depth, getOppIndex(player(state)), getOppIndex(player(state))), action) \
             for action in actions(state)]);
+
+class SimpleRandomAgent:
+    def getAction(self, state):
+        possibleActions = simplegame.actions(state);
+        numberActions = len(possibleActions);
+        randomAction = possibleActions[random.randrange(numberActions)];
+        return randomAction;
+
+class SimplePerceptronAgent:
+
+    def __init__(self):
+        self.weights = {"numWins" : 5, "numCenterPieces": .2, "numAdjacentPieces": .4, "numCornerPieces": .1};
+
+    def getAction(self, state):
+        possibleActions = simplegame.actions(state);
+        scoredActions = [];
+        for action in possibleActions:
+            successor = simplegame.succ(state, action);
+            scoredActions.append((dotProduct(self.simpleFeatureExtractor(successor), self.weights), action));
+        return randomMax(scoredActions);
+
+    def simpleFeatureExtractor(self, state):
+        features = collections.defaultdict(float);
+        otherPlayer = getOppIndex(player(state));
+        features["numWins"] = 1 if simplegame.playerWins(state)[1] == otherPlayer + 1 else 0;
+        features["numCenterPieces"] = 1 if state[0][1][1] == otherPlayer else 0;
+        features["numCornerPieces"] = simpleCountCornerMoves(state, otherPlayer);
+        features["numAdjacentPieces"] = simpleAdjacentMoves(state, otherPlayer);
+        return features;
