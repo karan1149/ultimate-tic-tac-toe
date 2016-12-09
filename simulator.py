f5d57dd02fbda6c018375fbb075901bdab545214
karan1149 <karan1149@gmail.com>
2016-12-08 17:57:13 -0800

    Playing around with what to simulate

--- simulator.py
+++ simulator.py
@@ -2,9 +2,16 @@ from simulate import *
 from game import *
 from agent import *
 
-agent1 = MinimaxPruningAgent(2);
+# agent1 = MinimaxPruningAgent(2);
+# # agent1 = RandomAgent();
+# agent2 = AdvancedPerceptronAgent();
+# simulator = TicTacToeSimulator(agent1, agent2);
+# print simulator.playGames(100);
+# print "minimax prungin2 vs advanced perceptron agent"
+
+agent1 = MinimaxPruningAgent(1);
 # agent1 = RandomAgent();
 agent2 = AdvancedPerceptronAgent();
 simulator = TicTacToeSimulator(agent1, agent2);
 print simulator.playGames(100);
-print "minimax prungin2 vs advanced perceptron agent"
+print "minimax prungin2 vs reg perceptron agent"
