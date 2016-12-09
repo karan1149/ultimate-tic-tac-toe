58a41347f9a0cc3bdb3550c3d67ce91985164a14
karan1149 <karan1149@gmail.com>
2016-12-08 23:14:58 -0800

    Started running tests, saved simple agent benchmarks

--- simulator.py
+++ simulator.py
@@ -2,17 +2,26 @@ from simulate import *
 from game import *
 from agent import *
 
-agent1 = SimplePerceptronAgent();
-agent2 = SimpleRandomAgent();
-simulator = TicTacToeSimulator(agent1, agent2);
-print simulator.playGames(10000);
 
-# agent1 = MinimaxPruningAgent(3);
-# # agent1 = RandomAgent();
-# agent2 = AdvancedPerceptronAgent();
-# simulator = UltimateTicTacToeSimulator(agent1, agent2);
-# print simulator.playGames(100);
-# print "minimax prungin2 vs advanced perceptron agent"
+# simple agent code
+# agent1 = SimplePerceptronAgent();
+# agent2 = SimpleRandomAgent();
+# simulator = TicTacToeSimulator(agent1, agent2);
+# print simulator.playGames(10000);
+# print "^ simple perceptron against simple random"; #.92, 0, .08
+
+# agent1 = SimpleRandomAgent();
+# agent2 = SimpleRandomAgent();
+# simulator = TicTacToeSimulator(agent1, agent2);
+# print simulator.playGames(10000);
+# print "^ simple random against simple random"; #.58, .13, .29
+
+agent1 = MinimaxPruningAgent(2);
+# agent1 = RandomAgent();
+agent2 = AdvancedPerceptronAgent();
+simulator = UltimateTicTacToeSimulator(agent1, agent2);
+print simulator.playGames(100);
+print "minimax prungin2 vs advanced perceptron agent"
 
 # agent1 = AdvancedPerceptronAgent();
 # agent2 = RandomAgent();
